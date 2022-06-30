717f6f5deec6c5b2064001f2407a8c508554a79f
Dario Gimenez <dario.s.gimenez@gmail.com>
2022-06-30 11:04:11 -0300

    Second Commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x712212d55CF4918956e1348dC7D891dC7cd4b936";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
